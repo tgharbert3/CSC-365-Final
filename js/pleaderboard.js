608988dcc9d21b9083e42e9e46bb94af59037524
Tyler Harbert <tyler.g.harbert@gmail.com>
2024-11-10 12:59:32 -0500

    inital commit to add all the current files

---
+++ js/pleaderboard.js
@@ -0,0 +1,19 @@
+import { teamArr } from "./premier_teams.js";
+const leaderboard = document.querySelector("tbody")
+
+
+for (let i = 0; i < teamArr.length; i++) {
+    let row = leaderboard.insertRow(i);
+    let cell = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    cell.innerHTML = teamArr[i].name;
+    cell1.innerHTML = teamArr[i].score;
+
+    if (i < 11) {
+        cell.style.backgroundColor = "LightGreen";
+        cell1.style.backgroundColor = "LightGreen";
+    } else {
+        cell.style.backgroundColor = "LightYellow";
+        cell1.style.backgroundColor = "LightYellow";
+    }
+}
---
+++ js/premier_teams.js
@@ -0,0 +1,41 @@
+export let teamArr = [];
+
+let make_team = (team_name, team_score) => {
+    let team = {
+        name: team_name,
+        score: team_score
+    }
+
+    teamArr.push(team);
+}
+
+make_team("UofC Exodus", 79)
+make_team("GCU Purple", 72);
+make_team("Southern Miss", 69);
+make_team("UQTR Black", 59);
+make_team("ISU Farmers", 58);
+make_team("UHE Varsity", 56);
+make_team("TAMU Maroon", 55.01);
+make_team("KSU Gold", 55);
+make_team("GCU Pookies", 52);
+make_team("CSULB Gold", 50);
+make_team("TMU Bold", 44);
+make_team("SDSU", 41.02);
+make_team("RIT Orange", 41.01);
+make_team("UH Zero", 41);
+make_team("SJSU Blue", 38);
+make_team("CSUDH Cardinal", 37)
+make_team("UT Orange", 36);
+make_team("TTU Red", 34);
+make_team("Ottawa Braves", 33);
+make_team("UU Red", 32);
+make_team("UB Esports", 27);
+make_team("KSU Black", 26.01);
+make_team("UCF Knights", 26);
+make_team("TU Cherry", 24);
+make_team("CSUF Blue",22.01);
+make_team("RIT Black", 22);
+make_team("SE Blue", 18);
+make_team("CSUF Orange", 16);
+make_team("St. Clair Saints", 13);
+make_team("UABESA Rift", 10);
